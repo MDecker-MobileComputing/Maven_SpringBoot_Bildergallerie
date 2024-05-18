5e6139eeb21b439ab4a1ed174d2df30513ea523e
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-05-18 11:30:23 +0200

    Projekt anleget mit Spring Initializr: https://bit.ly/springboot_bildergallerie

---
+++ src/main/java/de/eldecker/dhbw/spring/bildergallerie/BildergallerieApplication.java
@@ -0,0 +1,13 @@
+package de.eldecker.dhbw.spring.bildergallerie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BildergallerieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BildergallerieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/eldecker/dhbw/spring/bildergallerie/BildergallerieApplicationTests.java
@@ -0,0 +1,13 @@
+package de.eldecker.dhbw.spring.bildergallerie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BildergallerieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
