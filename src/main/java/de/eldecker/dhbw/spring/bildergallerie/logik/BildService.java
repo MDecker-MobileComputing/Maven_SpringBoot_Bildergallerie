fc1f0d2676918d547618fdbac6f47bf25c1246a3
MDecker-MobileComputing <mide_42b@yahoo.de>
2026-05-06 07:54:29 +0200

    Upgrade auf Spring Boot 4.x

--- src/main/java/de/eldecker/dhbw/spring/bildergallerie/logik/BildService.java
+++ src/main/java/de/eldecker/dhbw/spring/bildergallerie/logik/BildService.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.apache.tika.Tika;
-import org.hibernate.engine.jdbc.BlobProxy;
+import org.hibernate.engine.jdbc.proxy.BlobProxy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
